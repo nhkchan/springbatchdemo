00a2c77787cda8d1b10894167252ae573fd61d80
Ken <kenneth.cootauco@gmail.com>
2019-01-16 01:44:26 -0500

    Changes to BatchConfig + cleanup of mvn files

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,8 +1,16 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
 
+@EnableAutoConfiguration
+@Configuration
+@EnableConfigurationProperties
+@EnableCaching
 @SpringBootApplication
 public class Application {
 
--- src/main/java/hello/BatchConfiguration.java
+++ src/main/java/hello/BatchConfiguration.java
@@ -1,45 +1,33 @@
 package hello;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.LineAggregator;
-import org.springframework.batch.item.file.transform.FieldExtractor;
-import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
-import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-
-import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
+	private static final Logger log = LoggerFactory.getLogger(BatchConfiguration.class);
+	
 	@Autowired
-	EntityManagerFactory emf;
+	public DataSource datasource;
 	
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
@@ -47,119 +35,33 @@ public class BatchConfiguration {
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
 
-    private LineAggregator<VGSales> createVGSalesLineAggregator() {
-        DelimitedLineAggregator<VGSales> lineAggregator = new DelimitedLineAggregator<>();
-        lineAggregator.setDelimiter(",");
- 
-        FieldExtractor<VGSales> fieldExtractor = createVGSalesFieldExtractor();
-        lineAggregator.setFieldExtractor(fieldExtractor);
- 
-        return lineAggregator;
-    }
- 
-    private FieldExtractor<VGSales> createVGSalesFieldExtractor() {
-        BeanWrapperFieldExtractor<VGSales> extractor = new BeanWrapperFieldExtractor<>();
-        extractor.setNames(new String[] {"rank", "name", "platform","year","genre","publisher","naSales","euSales","jpSales","otherSales","globalSales"});
-        return extractor;
-    }    
+    JobLauncher jl;
     
-    ItemReader<Person> jdbcReader() {
-    	JdbcCursorItemReader<Person> dbReader = new JdbcCursorItemReader<>();
-    	
-		return null;
-    }
-    
-    // tag::readerwriterprocessor[]
-    public ItemReader<VGSales> reader() {
-    		FlatFileItemReader<VGSales> reader = new FlatFileItemReader<>();
-    		reader.setResource(new FileSystemResource("//Users//ken//Downloads//vgsales.csv"));
-    		reader.setLinesToSkip(1);
-            reader.setLineMapper(new DefaultLineMapper<VGSales>() {{
-                setLineTokenizer(new DelimitedLineTokenizer() {{
-                    setNames(new String[] {"rank", "name", "platform","year","genre","publisher","naSales","euSales","jpSales","otherSales","globalSales"});
-                }});
-                setFieldSetMapper(new BeanWrapperFieldSetMapper<VGSales>() {{
-                    setTargetType(VGSales.class);
-                }});
-            }});
-    		return reader;
-    }
-
-    @Bean
-    public VGSalesProcessor processor() {
-        return new VGSalesProcessor();
-    }
-
-    @Bean
-	public FlatFileItemWriter<VGSales> flatFileItemWriter() {
-		FlatFileItemWriter<VGSales> flatFileItemWriter = new FlatFileItemWriter<>();
-		flatFileItemWriter.setShouldDeleteIfExists(true);
-		flatFileItemWriter.setResource(new FileSystemResource("//Users//ken//Downloads//vgsales_writer.csv"));
-		
-		LineAggregator<VGSales> lineAggregator = createVGSalesLineAggregator();
-		flatFileItemWriter.setLineAggregator(lineAggregator);
-		return flatFileItemWriter;
-	}
+    CardsItemReader cir = new CardsItemReader();
     
     @Bean
-    public JpaItemWriter<VGSales> jpaWriter() {
-    	
-    	JpaItemWriter<VGSales> jpaWriter = new JpaItemWriter<>();
-    	jpaWriter.setEntityManagerFactory(emf);
-    	
-		return jpaWriter;
-    	
-    }
-    
-    // end::readerwriterprocessor[]
-    
-    @Bean
-/*    Job personEtl(JobBuilderFactory jobBuilderFactory,
-            StepBuilderFactory stepBuilderFactory,
-            FlatFileItemReader<Person> reader,
-            JdbcBatchItemWriter<Person> writer) {
-*/
-    Job personEtl(JobBuilderFactory jobBuilderFactory,
-    StepBuilderFactory stepBuilderFactory,
-    JdbcCursorItemReader<Person> reader) {
-    
-    	Step step = stepBuilderFactory.get("database-reader")
-    			.<Person, Person>chunk(5)
-    			.reader(reader)
+    public Job cardEtl(JobBuilderFactory jobBuilderFactory,
+    StepBuilderFactory stepBuilderFactory) throws UnexpectedInputException, ParseException, Exception {
+    	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    	Instant instant = timestamp.toInstant();
+    	Step step = stepBuilderFactory.get("database-reader-" + instant.getEpochSecond())
+    			.<Cards, Cards>chunk(10)
+    			.reader(cir.jdbcReader(datasource))
+    			.processor(new CardsItemProcessor())
+    			.writer(new CardsItemWriter())
     			.build();
     	
-    	Job job = jobBuilderFactory.get("etl")
-        .start(step)
-        .build();
+    	log.info("Step Name: " + step.getName());
     	
+    	Job job = jobBuilderFactory.get("pcf-sb-etl")
+    			.incrementer(new RunIdIncrementer())
+    			.flow(step)
+    			.end()
+		        .build();
+    	
+    	log.info("Job Name: " + job.getName());
     	return job;
-    	
     }
     
     
-    // Working Job/Step implementation
-    // tag::jobstep[]
-    /*
-    @Bean
-    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-        return jobBuilderFactory.get("importUserJob")
-            .incrementer(new RunIdIncrementer())
-            .listener(listener)
-            .flow(step1)
-            .end()
-            .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-            .<VGSales, VGSales> chunk(10)
-            .reader(reader())
-            .processor(processor())
-            //.writer(flatFileItemWriter())
-            .writer(jpaWriter())
-            .build();
-    }
-    */
-    // end::jobstep[]
 }
